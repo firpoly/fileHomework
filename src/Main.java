539be26acd2ebe21b19fc299c2e7a4a1aa569233
Firsova Polina <firpoly@gmail.com>
2024-02-07 00:53:27 +0300

    проврека дз

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static  StringBuilder log;
-    public static void writeLog(String path, String log){
+    public static StringBuilder log;
+
+    public static void writeLog(String path, String log) {
         try (FileWriter writer = new FileWriter(path, true)) {
             writer.append(log);
             writer.flush();
@@ -17,26 +16,28 @@ public class Main {
         }
 
     }
-    public static void makeDirectory(String path){
+
+    public static void makeDirectory(String path) {
         File dir = new File(path);
-        if (dir.exists()){
+        if (dir.exists()) {
             log.append(String.format("%s Каталог %s уже существует %n",
                     LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")).toString(),
                     path));
-        }else{
+        } else {
             if (dir.mkdir())
                 log.append(String.format("%s Каталог %s создан %n",
                         LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")).toString(), path));
         }
-       }
-    public static void makeFile(String path){
+    }
+
+    public static void makeFile(String path) {
         File myFile = new File(path);
         try {
-            if (myFile.exists()){
+            if (myFile.exists()) {
                 log.append(String.format("%s Файл %s уже существует %n",
                         LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")).toString(),
                         path));
-            }else{
+            } else {
                 if (myFile.createNewFile())
                     log.append(String.format("%s Файл %s создан %n",
                             LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")).toString(),
@@ -46,20 +47,21 @@ public class Main {
         } catch (IOException ex) {
             log.append(String.format("%s при создании файла %s, произошла ошибка %s %n",
                     LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")).toString(),
-                    path,ex.getMessage()));
+                    path, ex.getMessage()));
         }
     }
+
     public static void main(String[] args) {
         log = new StringBuilder();
-        makeDirectory( "Games/res");
-        makeDirectory( "Games/res/drawables");
-        makeDirectory( "Games/res/vectors");
-        makeDirectory( "Games/res/icons");
-        makeDirectory( "Games/src");
-        makeDirectory( "Games/src/main");
-        makeDirectory( "Games/src/test");
-        makeDirectory( "Games/savegames");
-        makeDirectory( "Games/temp");
+        makeDirectory("Games/res");
+        makeDirectory("Games/res/drawables");
+        makeDirectory("Games/res/vectors");
+        makeDirectory("Games/res/icons");
+        makeDirectory("Games/src");
+        makeDirectory("Games/src/main");
+        makeDirectory("Games/src/test");
+        makeDirectory("Games/savegames");
+        makeDirectory("Games/temp");
         makeFile("Games/temp/temp.txt");
         makeFile("Games/src/main/Main.java");
         makeFile("Games/src/main/Utils.java");
